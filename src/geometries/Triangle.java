ada150280cd68c7351354b460e715f8ae8d5fc00
haleli <haleli.alc@gmail.com>
2020-05-04 18:29:05 +0300

    recitation4

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -1,11 +1,6 @@
 package geometries;
 
 import primitives.Point3D;
-import primitives.Ray;
-import primitives.Util;
-import primitives.Vector;
-
-import java.util.List;
 
 /**
  * Triangae extends Polygon
@@ -25,6 +20,7 @@ public class Triangle extends Polygon {
 
         super(P1,P2,P3);
     }
+
     /*************** Admin *****************/
     @Override
     public String toString() {
@@ -32,41 +28,5 @@ public class Triangle extends Polygon {
                 "_vertices=" + _vertices +
                 ", _plane=" + _plane +
                 '}';
-    }
-
-    @Override
-    public List<Point3D> findIntersections(Ray ray)
-    {
-        List<Point3D> result=this._plane.findIntersections(ray);
-        if (result==null)
-            return null;
-       Vector v1 = this._vertices.get(0).subtract(ray.getP());
-       Vector v2 = this._vertices.get(1).subtract(ray.getP());
-       Vector v3 = this._vertices.get(2).subtract(ray.getP());
-
-       Vector N1=v1.crossProduct(v2).normalize();
-       Vector N2=v2.crossProduct(v3).normalize();
-
-       double t1= Util.alignZero(ray.getVector().dotProduct(N1));
-       double t2=Util.alignZero(ray.getVector().dotProduct(N2));
-
-       if(t1==0||t2==0)
-           return null;
-
-       if (t1>0)
-       {
-           if (t2>0)
-               return result;
-           if(t2<0)
-               return null;
-       }
-        if (t1<0)
-        {
-            if (t2<0)
-                return result;
-            if(t2>0)
-                return null;
-        }
-        return null;
     }
 }
